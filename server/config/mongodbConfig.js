ef811de77ae4da2432d45ea93f72bf89fb2db74e
ubaka2 <ubaka2@uic.edu>
2023-06-26 23:19:34 -0500

    Backend is ready

--- server/config/mongodbConfig.js
+++ server/config/mongodbConfig.js
@@ -4,8 +4,9 @@ require("dotenv").config();
 // Connect with database
 const connectWithDB = async () => {
   try {
-    if (await mongoose.connect(process.env.MONGODB_URI))
+    if (await mongoose.connect(process.env.MONGODB_URI)) {
       console.log("Connected with Database!!!");
+    }
   } catch (error) {
     console.log('*** Database Connection "FAILED" ***', error.message);
     process.exit(1);
