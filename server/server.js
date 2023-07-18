06fd97981d575d13c0c98b13758aefc9d2c4892a
ubaka2 <ubaka2@uic.edu>
2023-07-18 01:47:51 -0500

    Core changed

--- server/server.js
+++ server/server.js
@@ -14,8 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-app.use(cors());
+const corsOptions = {
+  credentials: true,
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
