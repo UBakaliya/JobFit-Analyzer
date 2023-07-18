4f9a27d9a753ea094b2c3ff435019bbdecffc175
ubaka2 <ubaka2@uic.edu>
2023-07-18 01:46:58 -0500

    Cookie access changed

--- server/server.js
+++ server/server.js
@@ -21,13 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // alow access to the client
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true"); // Allow sending cookies
+  // Allow sending cookies
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
