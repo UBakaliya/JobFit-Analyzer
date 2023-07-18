b919c21c31c8b81d11fbfebde4797879e15022e6
ubaka2 <ubaka2@uic.edu>
2023-07-18 01:08:21 -0500

    base url changed

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   // base url to make request to the server
-  axios.defaults.baseURL = "http://localhost:9999/api/v1/";
+  axios.defaults.baseURL = "https://jobfit-analyzer-server.onrender.com/api/v1/";
   
   useEffect(() => {
     const checkIsLoggedIn = async () => {
