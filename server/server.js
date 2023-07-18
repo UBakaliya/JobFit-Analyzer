2044556944eff283d53c841b9f4550412a7f1195
ubaka2 <ubaka2@uic.edu>
2023-07-18 01:24:37 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -14,22 +14,22 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// // alow access to the client
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Credentials", "true"); // Allow sending cookies
-//   next();
-// });
+// alow access to the client
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow sending cookies
+  next();
+});
 
 // Authentication routes
 app.use("/api/v1", authRoutes);
