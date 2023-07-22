8464dda558b0ed8da328bd428814a885e61a87fc
ubaka2 <ubaka2@uic.edu>
2023-07-21 22:20:52 -0500

    Application is ready for the deploy meant

--- server/model/resumeSchema.js
+++ server/model/resumeSchema.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-const ResumeSchema = mongoose.Schema(
-  {
-    fileName: { type: String, required: true },
-    contentType: { type: String, required: true },
-    size: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-  }
+const ResumeSchema = mongoose.Schema({
+  fileName: { type: String, required: true },
+  contentType: { type: String, required: true },
+  size: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-  // { timestamps: true }
-);
 const Resume = mongoose.model("Resume", ResumeSchema);
 
 module.exports = Resume;
